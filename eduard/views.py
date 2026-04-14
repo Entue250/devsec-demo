af33085049f4f9e503754db37e5496e54330fd72
Entue250 <edouardoniyomugabo@gmail.com>
2026-04-14 19:54:05 +0200

    Fixed Lint

--- eduard/views.py
+++ eduard/views.py
@@ -2,7 +2,6 @@ from django.contrib import messages
 from django.contrib.auth import login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.core.exceptions import PermissionDenied
 from .decorators import instructor_required
 
 from .forms import RegistrationForm, LoginForm, UserPasswordChangeForm
